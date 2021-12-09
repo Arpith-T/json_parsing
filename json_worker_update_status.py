11d9ba5063def9900836e7e19007951b6b6ee1a3
Arpith <arpith.t@sap.com>
2021-12-09 14:55:55 +0530

    Initial commit

---
+++ json_worker_update_status.py
@@ -0,0 +1,330 @@
+import json
+
+worker_update_status = {
+    "total": 40,
+    "updated": 39,
+    "inProgress": 0,
+    "failed": 1,
+    "tasks": [
+        {
+            "taskId": "0d662cb5-411f-4638-880d-d1c8e353d8d0",
+            "currentVersion": "",
+            "tenantName": "ops-test",
+            "status": "UPDATING",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "3211573b-9a13-41bf-ba23-c1576c5cf5f3",
+            "currentVersion": "",
+            "tenantName": "iatisuite2508",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "d5cc0ef2-ecfb-46c2-9704-98fd7ac9048e",
+            "currentVersion": "",
+            "tenantName": "iat-isuite-demo",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "223ca214-cc71-460d-a308-74506f16c257",
+            "currentVersion": "",
+            "tenantName": "petlandingpage1",
+            "status": "FAILED",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "433a3fa6-f3da-42fe-8680-70b1c41aafb0",
+            "currentVersion": "",
+            "tenantName": "az6210",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "ffb97b34-82f8-42e0-91dc-b2434688cc46",
+            "currentVersion": "",
+            "tenantName": "azats",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "f2bd173e-33f3-4a30-a384-661ec92655d3",
+            "currentVersion": "",
+            "tenantName": "isuitedemotest",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "87371c82-c77d-4ded-ab3f-94686ea0abea",
+            "currentVersion": "",
+            "tenantName": "isuite-demo-1",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "53dd7c52-0b9c-4e13-a6c9-438f7f117b9d",
+            "currentVersion": "",
+            "tenantName": "iat-be-pe",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "1ed5fb25-4581-473c-abf2-8eb359bd4dea",
+            "currentVersion": "",
+            "tenantName": "aziat61617",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "dd4e65e5-c645-4bd4-b239-6174ae53735a",
+            "currentVersion": "",
+            "tenantName": "iat-freemium",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "a1cd4106-57c6-4818-a3d4-e7e2e8ef9477",
+            "currentVersion": "",
+            "tenantName": "az103",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "32db01f4-4343-4c4c-9639-6512d65201f1",
+            "currentVersion": "",
+            "tenantName": "testfree",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "5a993a17-5470-411a-900b-a44fd0995608",
+            "currentVersion": "",
+            "tenantName": "az-auditlogging-test",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "327e2cd5-90d0-4a94-b9af-2bf954cf7897",
+            "currentVersion": "",
+            "tenantName": "petazure",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "a383aaa7-a5d3-4cca-a039-4620d2f449b0",
+            "currentVersion": "",
+            "tenantName": "integrationsuite-iat-cf-nl-azure-1005",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "687438ec-8194-4631-89cd-b7d6c4c71de5",
+            "currentVersion": "",
+            "tenantName": "isuite-endurance-tests",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "9f2ed7ee-4206-448b-a37e-249f85e0b79b",
+            "currentVersion": "",
+            "tenantName": "ztpmiat01",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "3766d663-3090-43c5-a4d8-e07223711b8a",
+            "currentVersion": "",
+            "tenantName": "lagsharding",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "b097a4c1-84e8-4360-b133-edc3ae8da432",
+            "currentVersion": "",
+            "tenantName": "az61812",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "948b52a4-58a3-4d3c-9642-c832502c316b",
+            "currentVersion": "",
+            "tenantName": "az2006iat",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "f2d80382-1eb7-4e2a-82e7-6de1065b3681",
+            "currentVersion": "",
+            "tenantName": "suitee2edemogk",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "224f6bd3-4b1a-4456-92b5-a0b680da9ee8",
+            "currentVersion": "",
+            "tenantName": "pv-aziat-61811",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "6f627780-a8e0-46a7-8c7c-143596561952",
+            "currentVersion": "",
+            "tenantName": "iat-tpm",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "0aa75091-b113-4b1c-a5a2-c4d9095d9997",
+            "currentVersion": "",
+            "tenantName": "saba-kt",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "dc5c0d7b-d25d-4b26-8223-7a04748e3412",
+            "currentVersion": "",
+            "tenantName": "saffron-innoweek-zdev",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "b601c7df-a0f9-4c10-8201-62e037abc553",
+            "currentVersion": "",
+            "tenantName": "chrome-iat-testtenant",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "e9a804e5-6783-498f-8f1e-c53a72ebd210",
+            "currentVersion": "",
+            "tenantName": "az6170",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "274094bb-bedd-42d4-8d84-9cdb0d69c979",
+            "currentVersion": "",
+            "tenantName": "az-manual",
+            "status": "FAILED",
+            "retryable": True,
+            "errorDescription": "null message java.util.concurrent.TimeoutException"
+        },
+        {
+            "taskId": "bfeff896-8983-45d8-8291-de6dcd510b46",
+            "currentVersion": "",
+            "tenantName": "az105",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "b453ff8d-86b3-4985-94f2-b45693fc974a",
+            "currentVersion": "",
+            "tenantName": "az-rt6186",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "5743456d-3e1b-4624-a0a1-569a0d54e610",
+            "currentVersion": "",
+            "tenantName": "az-infra",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "e2e246d5-e5f6-472e-9666-7af834d7b049",
+            "currentVersion": "",
+            "tenantName": "az2007iatrel",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "50e9f62d-f018-4cb8-8497-6a416f7452d7",
+            "currentVersion": "",
+            "tenantName": "ztpmiat02",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "9a931381-8d1a-406e-8410-9d064259e339",
+            "currentVersion": "",
+            "tenantName": "mysubdomain",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "aa97d967-94f8-4882-8f6e-af89aea33114",
+            "currentVersion": "",
+            "tenantName": "az104",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "dbe81443-3f99-43ab-900d-3dc594752478",
+            "currentVersion": "",
+            "tenantName": "az102",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "f62e9211-6043-4340-9a67-c0b3c3250b25",
+            "currentVersion": "",
+            "tenantName": "az-slc",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "f9464dc5-ab0d-4ff4-89fb-08ac91d8761c",
+            "currentVersion": "",
+            "tenantName": "koloncpis",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        },
+        {
+            "taskId": "7e491ac9-f4a6-4d2e-8be6-c73570666d25",
+            "currentVersion": "",
+            "tenantName": "azrit02",
+            "status": "SUCCESS",
+            "retryable": True,
+            "errorDescription": ""
+        }
+    ]
+}
